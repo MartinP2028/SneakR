bf759f6a9522b88673b9bf1f1950a41e2dc00d6b
MartinP2028 <martin.pinaud@epitech.digital>
2023-12-07 10:28:55 +0100

    [add] first version of navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
   <section>
+    <div>
+      <Navbar />
+      <SearchBar />
+      <!-- Rest of your template -->
+    </div>
     <h1>Home page site de sneakers</h1>
   </section>
 </template>
