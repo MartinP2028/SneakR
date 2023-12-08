1c1743a1f640cb1a2258285f2d95fa26d66e0dcd
MartinP2028 <martin.pinaud@epitech.digital>
2023-12-08 12:08:28 +0100

    [add] product on home page and [upd] searchbar

---
+++ components/List_sneakers.vue
@@ -0,0 +1,13 @@
+<template>
+  <article>
+    <img class="img" :src="Sneakers['image.small']" alt="image of sneaker" />
+    <p class="name">{{ Sneakers.name }}</p>
+    <p class="price">${{ Sneakers.estimatedMarketValue }}</p>
+    <button class="bg-gray-200 mr-2">ADD TO WISHLIST</button>
+    <button class="bg-gray-200">ADD TO COLLECTION</button>
+  </article>
+</template>
+
+<script setup>
+const props = defineProps(["Sneakers"]);
+</script>
