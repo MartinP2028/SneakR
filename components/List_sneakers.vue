4d0fd67e2116c299cf92b93aa8e3dda12652a0de
MartinP2028 <martin.pinaud@epitech.digital>
2023-12-09 14:55:30 +0100

    [upd] front of the list of sneakers

--- components/List_sneakers.vue
+++ components/List_sneakers.vue
@@ -1,18 +1,40 @@
 <template>
-    <article class="grid">
-        <div class="element bg-gray-200 p-4 rounded-lg shadow-md">
-            <div class="img w-full h-64 rounded-lg">
-                <img v-if="Sneakers['image.small']" class="w-full h-full object-cover rounded-lg" :src="Sneakers['image.small']" alt="image of sneaker" />
-            </div>
-            <p class="name text-xl font-bold mt-2">{{ Sneakers.name }}</p>
-            <p class="colorway text-gray-500">{{ Sneakers.colorway }}</p>
-            <p class="price text-gray-500 font-bold mt-2">${{ Sneakers.estimatedMarketValue }}</p>
-            <button class="bg-black text-white mt-4 py-2 px-4 rounded-lg">ADD TO WISHLIST</button>
-            <button class="bg-black text-white mt-2 py-2 px-4 rounded-lg">ADD TO COLLECTION</button>
-        </div>
-    </article>
+  <article class="grid">
+    <div class="element bg-gray-200 p-4 rounded-lg shadow-md flex flex-col">
+      <div class="img w-full h-full rounded-lg">
+        <img
+          v-if="Sneakers['image.small']"
+          class="img w-full h-full rounded-lg"
+          :src="Sneakers['image.small']"
+          alt="image of sneaker"
+        />
+      </div>
+      <div class="product_infos flex flex-col">
+        <p class="name text-xl font-bold mt-2">{{ Sneakers.name }}</p>
+        <p class="colorway text-gray-500">{{ Sneakers.colorway }}</p>
+        <p class="price text-gray-500 font-bold mt-2">
+          ${{ Sneakers.estimatedMarketValue }}
+        </p>
+      </div>
+      <div class="add_to_buttons flex flex-col">
+        <button class="bg-black text-white mt-2 py-2 px-4 rounded-lg">
+          ADD TO COLLECTION
+        </button>
+        <button
+          @click="addToWishlist()"
+          class="bg-black text-white mt-4 py-2 px-4 rounded-lg"
+        >
+          ADD TO WISHLIST
+        </button>
+      </div>
+    </div>
+  </article>
 </template>
 
 <script setup>
 const props = defineProps(["Sneakers"]);
+function addToWishlist() {
+  console.log("add to wishlist");
+  console.log(props.Sneakers);
+}
 </script>
