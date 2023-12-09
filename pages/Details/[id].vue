8b2744ed439ff441142e48dc8444231e9d0f3713
MartinP2028 <martin.pinaud@epitech.digital>
2023-12-09 16:40:17 +0100

    [add] details id page

---
+++ pages/Details/[id].vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <div>
+      <Navbar />
+      <Searchbar />
+      <p class="text-lg">vous etes sur la page details [id]</p>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 bg-gray-100">
+        <ListSneakers {{ route.params.id  }}
+          class="border rounded-lg border-shadow-2"
+        />
+      </div>
+      <Pagination />
+    </div>
+  </section>
+</template>
+
+<script setup>
+const client = useSupabaseClient();
+const route = useRoute();
+
+const { data: Sneaker } = await useAsyncData("Sneaker", async () => {
+  const { data } = await client
+    .from("Sneaker")
+    .select("['image.small'], name, estimatedMarketValue, colorway")
+    .order("brand")
+    .range(0, 11)
+    .eq("id", route.params.id);
+  return data;
+});
+</script>
