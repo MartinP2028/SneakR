06f4f609f66bb316feeb50bf1d047e9a4c5a29a8
MartinP2028 <martin.pinaud@epitech.digital>
2023-12-11 13:54:08 +0100

    [upd] register function

--- pages/Login.vue
+++ pages/Login.vue
@@ -21,7 +21,7 @@
                 type="email"
                 autocomplete="email"
                 required
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm hover:border-indigo-500 hover:ring-indigo-500"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none"
                 placeholder="Email address"
               />
             </div>
@@ -33,7 +33,7 @@
                 type="password"
                 autocomplete="current-password"
                 required
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm hover:border-indigo-500 hover:ring-indigo-500"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none"
                 placeholder="Password"
               />
             </div>
@@ -45,7 +45,7 @@
                 id="remember_me"
                 name="remember_me"
                 type="checkbox"
-                class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                class="h-4 w-4 text-indigo-600 border-gray-300 rounded"
               />
               <label for="remember_me" class="ml-2 block text-sm text-gray-900">
                 Remember me
@@ -65,7 +65,7 @@
           <div>
             <button
               type="submit"
-              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-gray-800 focus:outline-none"
             >
               Sign in
             </button>
@@ -74,7 +74,7 @@
 
         <div class="text-sm">
           <p class="mt-2">
-            Don\'t have an account ?
+            Don't have an account ?
             <a
               href="/register"
               class="font-medium text-indigo-600 hover:text-indigo-500"
--- pages/Register.vue
+++ pages/Register.vue
@@ -1,51 +1,26 @@
 <template>
   <div>
     <Navbar />
-    <section
-      class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
-    >
+    <section class="flex items-center justify-center py-100 sm:px-6 lg:px-8">
       <div class="max-w-md w-full space-y-8">
         <div>
           <h2 class="mt-6 text-center text-3xl font-extrabold">
             Register an account
           </h2>
         </div>
-        <form class="mt-8 space-y-6" action="#" method="POST">
+        <div class="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" value="true" />
           <div class="rounded-md shadow-sm -space-y-px">
-            <div>
-              <label for="first-name" class="sr-only">First Name</label>
-              <input
-                id="first-name"
-                name="first-name"
-                type="text"
-                autocomplete="given-name"
-                required
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm hover:border-indigo-500 hover:ring-indigo-500 hover:shadow-md"
-                placeholder="First Name"
-              />
-            </div>
-            <div>
-              <label for="last-name" class="sr-only">Last Name</label>
-              <input
-                id="last-name"
-                name="last-name"
-                type="text"
-                autocomplete="family-name"
-                required
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm hover:border-indigo-500 hover:ring-indigo-500 hover:shadow-md"
-                placeholder="Last Name"
-              />
-            </div>
             <div>
               <label for="email-address" class="sr-only">Email address</label>
               <input
                 id="email-address"
+                v-model="email"
                 name="email"
                 type="email"
                 autocomplete="email"
                 required
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm hover:border-indigo-500 hover:ring-indigo-500 hover:shadow-md"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 sm:text-sm hover:border-indigo-500 hover:shadow-md"
                 placeholder="Email address"
               />
             </div>
@@ -53,26 +28,13 @@
               <label for="password" class="sr-only">Password</label>
               <input
                 id="password"
+                v-model="password"
                 name="password"
                 type="password"
                 autocomplete="new-password"
                 required
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm hover:border-indigo-500 hover:ring-indigo-500 hover:shadow-md"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 sm:text-sm hover:border-indigo-500 hover:shadow-md"
                 placeholder="Password"
-              />
-            </div>
-            <div>
-              <label for="confirm-password" class="sr-only"
-                >Confirm Password</label
-              >
-              <input
-                id="confirm-password"
-                name="confirm-password"
-                type="password"
-                autocomplete="new-password"
-                required
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm hover:border-indigo-500 hover:ring-indigo-500 hover:shadow-md"
-                placeholder="Confirm Password"
               />
             </div>
           </div>
@@ -80,12 +42,13 @@
           <div>
             <button
               type="submit"
-              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:shadow-md"
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-gray-800 focus:outline-none hover:shadow-md"
+              @click="createUser()"
             >
               Register
             </button>
           </div>
-        </form>
+        </div>
 
         <div class="text-sm">
           <p class="mt-2">
@@ -102,3 +65,17 @@
     </section>
   </div>
 </template>
+
+<script setup lang="ts">
+const email = ref("");
+const password = ref("");
+const supabase = useSupabaseClient();
+async function createUser() {
+  const { data, error } = await supabase.auth.signUp({
+    email: email.value,
+    password: password.value,
+  });
+  console.log(data);
+  console.error(error);
+}
+</script>
