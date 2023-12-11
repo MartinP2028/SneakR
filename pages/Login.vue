443f5fdbb6962216a36f0b0ba685210e636ee8c4
MartinP2028 <martin.pinaud@epitech.digital>
2023-12-11 16:25:25 +0100

    [upd] front of login to do the same as register

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <Navbar />
-    <section
-      class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
-    >
+    <section class="flex items-center justify-center py-100 sm:px-6 lg:px-8">
       <div class="max-w-md w-full space-y-8">
         <div>
-          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          <h2 class="mt-6 text-center text-3xl font-extrabold">
             Sign in to your account
           </h2>
         </div>
-        <form class="mt-8 space-y-6" action="#" method="POST">
+        <div class="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" value="true" />
           <div class="rounded-md shadow-sm -space-y-px">
             <div>
@@ -21,7 +19,7 @@
                 type="email"
                 autocomplete="email"
                 required
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 sm:text-sm hover:border-indigo-500 hover:shadow-md"
                 placeholder="Email address"
               />
             </div>
@@ -31,46 +29,23 @@
                 id="password"
                 name="password"
                 type="password"
-                autocomplete="current-password"
+                autocomplete="new-password"
                 required
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 sm:text-sm hover:border-indigo-500 hover:shadow-md"
                 placeholder="Password"
               />
-            </div>
-          </div>
-
-          <div class="flex items-center justify-between">
-            <div class="flex items-center">
-              <input
-                id="remember_me"
-                name="remember_me"
-                type="checkbox"
-                class="h-4 w-4 text-indigo-600 border-gray-300 rounded"
-              />
-              <label for="remember_me" class="ml-2 block text-sm text-gray-900">
-                Remember me
-              </label>
-            </div>
-
-            <div class="text-sm">
-              <a
-                href="#"
-                class="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                Forgot your password ?
-              </a>
             </div>
           </div>
 
           <div>
             <button
               type="submit"
-              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-gray-800 focus:outline-none"
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-gray-800 focus:outline-none hover:shadow-md"
             >
               Sign in
             </button>
           </div>
-        </form>
+        </div>
 
         <div class="text-sm">
           <p class="mt-2">
