8eeceb23d2c2692cb7f1bbe1526683756d793865
MartinP2028 <martin.pinaud@epitech.digital>
2023-12-12 10:09:16 +0100

    [upd] nuxt config to prefer light mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase', '@nuxt/ui', '@nuxtjs/tailwindcss'],
+  colorMode: {
+    preference: "light",
+  },
+  modules: ["@nuxtjs/supabase", "@nuxt/ui", "@nuxtjs/tailwindcss"],
   supabase: {
     redirectOptions: {
-      login: '/login',
-      callback: '/',
-      exclude: ['/', "/login", "/register", "/confirm", "/details/*", "/forgot_password", "/update_password"],
-    }
+      login: "/login",
+      callback: "/",
+      exclude: [
+        "/",
+        "/login",
+        "/register",
+        "/confirm",
+        "/details/*",
+        "/forgot_password",
+        "/update_password",
+      ],
+    },
   },
-})
+});
